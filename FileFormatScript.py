14a53bb33779054cc5438197c00031d23fbf1316
dangeles <dangeles@caltech.edu>
2014-11-14 12:04:50 -0800

    3rd Commit, FileFormatScript.py improved and debugged

--- FileFormatScript.py
+++ FileFormatScript.py
@@ -1,71 +1,73 @@
 # -*- coding: utf-8 -*-
 """
 Created on Mon Nov  3 13:28:07 2014
-A script to re-format a couple of files that were given me by Margie into the adequate format.
-These scripts are SWAG1 and SWAG2 gene expression profiles. 
+A script to re-format a file
 @author: davidaalbores
 """
 from __future__ import division, print_function, absolute_import
 import pandas as pd
 
+#Filenames!
+fileInput= '../input/glp-4dissectedgut(SWAG1).txt'; sepInput= '\t'; comInput= '#';
+newFile= '../input/experimentalGE.txt'
+errorFile= '../output/ErrorFile.txt'
+summaryFile= '../output/Summary.txt'
+
+#Filename and location of the dataframe you want to translate, 
+#kind of separator used and comment marker
+filename= '../input/c_elegans.PRJNA13758.WS241.livegeneIDs.unmaprm.txt'
+separator= "\t"
+comments= "#"
+
 #Load the gene name dictionary
-names= pd.read_csv("../input/c_elegans.PRJNA13758.WS241.livegeneIDs.unmaprm.txt", sep= "\t", comment= "#")
+names= pd.read_csv(filename, sep= separator, comment= comments)
 
 #Load the control database
-control= pd.read_csv("../input/AVAcontrol.txt", sep= "\t", comment= "#")
-control.columns= ['gene', 'reads']
-##Experimental
-#experimental= pd.read_csv("../input/glp-4dissectedgut(SWAG1).txt", sep= "\t", comment= "#")
+df= pd.read_csv(fileInput, sep= sepInput, comment= comInput)
+df= df.loc[:, ['gene', 'reads'] ]
+#df.columns= ['gene', 'reads']
 
-#Discard the first line of the dataframes; it's useless. 
-#control= control[1:]
-#experimental= experimental[1:]
-
-#Make a new dataframe that only contains the counts and the gene name, dropping any NAs
-control= control.loc[:, ['gene', 'reads']].dropna()
-#experimental= experimental.loc[:, ['SWAG1', 'gene']].dropna()
+nInitial= len(df)
+nulls= []
 
 #change the names into WB IDs
-for i in range(0, len(control)):
+for i in range(0, len(df)):
     #get the gene associated with locus i
-    g= control.iloc[i].gene
+    g= df.iloc[i].gene
     if names[names.GeneName == g].empty == False:
-       #make a variable x that extracts the worm ID from the name df. this is a
-    #dataframe itself!
-        print('here')
+        #make a variable x that extracts the worm ID from the name df. this is a
+        #dataframe itself!
         x= names[names.GeneName == g].WBID 
         #Replace the gene name in the control array        
-        control= control.replace(g, x.iloc[0])
+        df= df.replace(g, x.iloc[0])
     else:
         if names[names.WBID == g].empty == True:
-            #There's going to be a problem.
-            print(names[names.WBID == g])
-            control= control.replace(g, float('nan'))
-        else:
-            print(names[names.WBID == g])
+            if names[names.HumanReadable == g].empty == False and g.lower() != 'nan':
+                x= names[names.HumanReadable == g].WBID
+                df= df.replace(g, x)
+            else:
+                #There's going to be a problem.
+                nulls.append([g, df.iloc[0].reads])
+                df= df.replace(g, float('nan'))
 
-##change the names into WB IDs
-#for i in range(0, len(experimental)):
-#    #get the gene associated with locus i
-#    g= experimental.iloc[i].gene
-#    if names[names.GeneName == g].empty == False:
-#       #make a variable x that extracts the worm ID from the name df. this is a
-#    #dataframe itself!
-#        x= names[names.GeneName == g].WBID 
-#        #Replace the gene name in the experimental array        
-#        experimental= experimental.replace(g, x.iloc[0])
-#    else:
-#        #There's going to be a problem.
-#        experimental= experimental.replace(g, float('nan'))
+df= df.dropna()
 
-    
-control= control.dropna()
-#experimental= experimental.dropna()
+#Write to main file
+df.to_csv(newFile, sep=',', index= False)
 
+#Write error file
+f= open(errorFile, 'w')
+f.write("#This file stores any genes that could not be translated into a WBID\n")
+f.write("gene_Name\,reads\n")
+for element in nulls:
+    string= str(element[0])+","+str(element[1])+"\n"
+    f.write(string)
+f.close()
 
-control.to_csv("../input/controlAVA131014.txt", sep=',')
-#experimental.to_csv("experimentalGE.txt", sep=',')
-
-
-
+#Write summary
+f1= open(summaryFile, 'w')
+f1.write("#This file gives a summary of the results of running FileFormatScript.py\n")
+n= len(df)
+f1.write("Original list contained {0} entries. List with WBIDs contains {1} entries".format(nInitial, n))
+f1.close()
 
